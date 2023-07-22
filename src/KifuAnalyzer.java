e14ac76ed5c9f9b0e7980d964ebe1787bce872c0
ShogiRamenTV <shogi.ramen@gmail.com>
2023-07-23 07:50:44 +0900

    Change Application Name

--- src/KifuAnalyzer.java
+++ src/KifuAnalyzer.java
@@ -118,7 +118,7 @@ public class KifuAnalyzer extends JFrame implements MouseListener, MouseMotionLi
 
 	public static void main(String[] args) {
 		KifuAnalyzer ka = new KifuAnalyzer();
-		ka.setTitle("ShogiRamenApp");
+		ka.setTitle("KifuAnalyzer");
 		ka.setVisible(true);
 		ka.shogiData.viewKomaOnBoard();
 	}
